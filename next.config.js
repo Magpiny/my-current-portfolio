c814ccab6506c8b3c894a2481b5021ec4f4008e7
Magpiny <magpinyb@gmail.com>
2024-12-17 15:28:58 +0300

    Changed from babel to swc DELETD: .babelrc and replaced it with next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true, // Optional: Enable React Strict Mode
+
+    compiler: {
+        styledComponents: true,
+    },
+}
+
+module.exports = nextConfig
